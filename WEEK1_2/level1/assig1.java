972493ab620125387b89f1799d1cdb7d5bf5eac6
mahaaharish112-cyber <mahaaharish112@gmail.com>
2026-03-10 09:43:09 +0530

    Added WEEK1_2 level programs

---
+++ WEEK1_2/level1/assig1.java
@@ -0,0 +1,12 @@
+public class assig1 {
+    public static void main(String[] args) {
+        
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+        
+        double average = (maths + physics + chemistry) / 3.0;
+        
+        System.out.println("Sam average mark in PCM is " + average + "%");
+    }
+}
